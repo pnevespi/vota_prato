34ef8a5c1230471b2d779d0cfcf7df04d75a9504
paulo.neves <pnevespi@gmail.com>
2013-12-27 19:05:47 -0300

    acrescimo de mais arquivos do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = '1931874a8d5749d1ccec36ef4ef95ed245b63f27595a5beb7452e2b2e26952f3d033a752cdd85148d6f919e044901c8c20d8038430ff81ff8b476b5e2a7f4a43'
