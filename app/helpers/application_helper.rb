a06f32aa9816917f377da7f8ab6c9ffb30c7a6cb
paulo.neves <pnevespi@gmail.com>
2013-12-30 08:54:37 -0300

    Resolução do exercício 13.6 - listando os comentários de um comentável

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def novo_comentario(comentavel)
     render partial: "comentarios/novo_comentario", locals:{comentavel: comentavel}
   end
+
+  def comentarios(comentavel)
+    render partial: "comentarios/comentarios", locals:{comentarios: comentavel.comentarios}
+  end
 end
